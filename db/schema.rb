25b49edb641a549564653005ac7c296ad4504af0
lillccian <lillccian@gmail.com>
2016-11-18 17:11:28 +0800

    set relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118073941) do
+ActiveRecord::Schema.define(version: 20161118090504) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
