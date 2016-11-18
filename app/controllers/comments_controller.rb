8e5145a3dcd307ab27e564be5481a2fd8ce03610
lillccian <lillccian@gmail.com>
2016-11-18 18:02:17 +0800

    add comment edit and set comment show in post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,23 @@ class CommentsController < ApplicationController
 	def show
 		@comment = @post.comments.find(params[:id])
 	end
+	def edit
+		@comment = @post.comments.find(params[:id])
+	end
+	def update
+		@comment = @post.comments.find(params[:id])
+		if @comment.update(comment_params)	
+			redirect_to post_path(@post)
+		else
+			render :action =>:edit
+		end
+	end
+
 	private 
 	def find_post
-	  @post = Post.find( params[:post_id] )
+	  @post = Post.find(params[:post_id])
+	end
+	def comment_params
+		params.require(:comment).permit(:name,:description)
 	end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 	end
 	def show
 		@post = Post.find(params[:id])
+		@comments=@post.comments
 	end
 	def edit
 		@post = Post.find(params[:id])	
